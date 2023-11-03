a3f0a0be3ba3bd4e09011cf844a9d399c6a3071a
AmelioMansour <ameliomansour@oakland.edu>
2023-11-02 20:39:07 -0400

    updated name, added email templates

--- src/components/how.js
+++ src/components/how.js
@@ -13,31 +13,26 @@ function HowItWorks() {
       <section className="mb-6">
         <h2 className="text-3xl font-semibold mb-4">Step 1: Data Collection</h2>
         <p className="text-lg">
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
       </section>
       <section className="mb-6">
         <h2 className="text-3xl font-semibold mb-4">Step 2: Data Processing</h2>
         <p className="text-lg">
-          Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
       </section>
+      
       <section className="mb-6">
-        <h2 className="text-3xl font-semibold mb-4">Step 3: Model Training</h2>
+        <h2 className="text-3xl font-semibold mb-4">Step 3: Email Detection</h2>
         <p className="text-lg">
-          Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam.
-        </p>
-      </section>
-      <section className="mb-6">
-        <h2 className="text-3xl font-semibold mb-4">Step 4: Email Detection</h2>
-        <p className="text-lg">
-          Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
       </section>
       <section className="mb-6">
         <h2 className="text-3xl font-semibold mb-4">Conclusion</h2>
         <p className="text-lg">
-          Cras convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
       </section>
     </div>
